d01b1608bc82c95aded49ee52e844f22c10ac0c6
Dovydas Venckus <dovven2@ktu.lt>
2024-01-02 22:55:21 +0200

    Door opening attempt 1

--- Assets/Scripts/CheckSolution.cs
+++ Assets/Scripts/CheckSolution.cs
@@ -8,7 +8,15 @@ public class CheckSolution : MonoBehaviour
     [SerializeField]
     List<GameObject> Tables;
 
+    [SerializeField]
+    GameObject DoorHinge;
+
+    [SerializeField]
+    float DoorOpenDuration;
+
     List<GameObject> TableItems = new List<GameObject>() {null, null, null, null, null};
+
+
 
     public void Add(SelectEnterEventArgs args)
     {
@@ -47,6 +55,37 @@ public class CheckSolution : MonoBehaviour
         if(solved)
         {
             // Solution correct, do something
+
+            // Open door, enable level switching
+            StartCoroutine(DoorOpenRoutine());
         }
+    }
+
+    public IEnumerator DoorOpenRoutine()
+    {
+        float timer = 0;
+        while (timer <= DoorOpenDuration)
+        {
+            var newRotation = new Vector3(0,0,0);    
+            newRotation.y = Mathf.Lerp(0, -90, timer/DoorOpenDuration);
+            var newPosition = new Vector3(-0.469f, 1.5f, 0);
+            newPosition.x = Mathf.Lerp(-0.469f, -1.689f, timer / DoorOpenDuration);
+            newPosition.z = Mathf.Lerp(0, 1.24f, timer / DoorOpenDuration);
+
+            DoorHinge.transform.localEulerAngles = newRotation;
+            DoorHinge.transform.localPosition = newPosition;
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        var newRotation2 = new Vector3(0, 0, 0);
+        newRotation2.y = -90;
+        var newPosition2 = new Vector3(-0.469f, 1.5f, 0);
+        newPosition2.x = -1.689f;
+        newPosition2.z = 1.24f;
+
+        DoorHinge.transform.localEulerAngles = newRotation2;
+        DoorHinge.transform.localPosition = newPosition2;
     }
 }
